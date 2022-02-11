6aeeb45ae3d48261fdcd0c67dd9839492ac20c51
Elisa <elitodd@ucm.es>
2022-02-12 00:03:33 +0100

    Sigue cuajao :(

--- MonkeyDelivery/Src/View/TextureContainer.h
+++ MonkeyDelivery/Src/View/TextureContainer.h
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-const string IMAGES_PATH = "C:/Users/ADRIAN/Desktop/2.Carrera/2ºCUATRI/MonkeyDelivery/MonkeyDelivery/images/";
+const string IMAGES_PATH = "../images/";
 
 enum TextureName {
     monkeyTexture
