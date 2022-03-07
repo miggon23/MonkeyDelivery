c859c26b0cf1c46c6862c6c746a519edc02833af
sarais02 <sarais02@ucm.es>
2022-03-07 17:18:46 +0100

    El toro ya da miedo, COMPLETADO

--- MonkeyDelivery/Src/Logic/Bull.cpp
+++ MonkeyDelivery/Src/Logic/Bull.cpp
@@ -10,6 +10,7 @@ Bull::Bull(Game* game, int Aleatorio, Point2D<int> centroRadio) : Enemy(game, Al
 	initialPos_ = getPosition();
 	createCheckPoints();
 	stop = true; //Patrulla y no persigue
+	setScariness(0.14);
 }
 
 void Bull::update()
