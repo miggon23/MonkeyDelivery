c9e179ad40953281ba4f9cc636a0d615aeba0d11
Jacobo0601 <jacoboal@ucm.es>
2022-03-14 16:24:28 +0100

    Importada Carpeta Tiled a Main

---
+++ MonkeyDelivery/Src/TilemapSrc/FloorTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="FloorTileset" tilewidth="16" tileheight="16" tilecount="572" columns="22">
+ <image source="Spritesheets/TilesetFloor.png" width="352" height="417"/>
+</tileset>
