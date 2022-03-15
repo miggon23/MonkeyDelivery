26436e791c8dee69a282a4ba841f5eb041a6746b
RaulSaavedraRiera <rsaave02@ucm.es>
2022-03-15 17:56:11 +0100

    ajustado dibujo en la tienda y probado
    falta comprobar con sprites finales

--- MonkeyDelivery/Src/Control/States/ShopState.cpp
+++ MonkeyDelivery/Src/Control/States/ShopState.cpp
@@ -25,19 +25,25 @@ void ShopState::draw()
 {
 	//game->renderText("PAUSED", game->getWindowWidth() / 2 - 75, game->getWindowHeight() / 2 - 50);
 	
-	SDL_Rect rectPanel = { xPanel,yPanel,wPanel,hPanel };
+	SDL_Rect rectPanel = { 0,0,game->getWindowWidth(),game->getWindowHeight() };
 	panelTexture->render(rectPanel);
 
 	int i = 0;
-	while (i < shop_->getSize() && i < 4)
+
+	//pruebas
+	while (i < 2)
 	{
-		shop_->objects[i]->getTexture()->render({ xObj * i, yObj, wObj, hObj });
+		game->getTexture(bullTexture)->render({ xOffset + xObj * i, yOffset, wObj, hObj });
 		i++;
 	}
+	game->getTexture(bullTexture)->render({ xOffset, yOffset + yObj, wObj, hObj });
+	//
 
+	//forma render inal
+	
 	while (i < shop_->getSize())
 	{
-		shop_->objects[i]->getTexture()->render({ xObj * (i-4), yObj * 2, wObj, hObj });
+		shop_->objects[i]->getTexture()->render({ xOffset + xObj * (i-4), yOffset + yObj, wObj, hObj });
 		i++;
 	}
 	
--- MonkeyDelivery/Src/Control/States/ShopState.h
+++ MonkeyDelivery/Src/Control/States/ShopState.h
@@ -5,8 +5,8 @@ class Shop;
 class ShopState : public State
 {
 	Texture* panelTexture;
-	int xPanel=100, yPanel=50, wPanel=500, hPanel=250;
-	int xObj=100, yObj=50, wObj=100, hObj=100, xOffset = 100, yOffset = 100;
+	
+	int xObj=140, yObj=105, wObj=100, hObj=100, xOffset = 160, yOffset = 115;
 
 	Shop* shop_;
 	
