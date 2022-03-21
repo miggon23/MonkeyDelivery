618a3a50632df79614879a1264479f544109a32c
sarais02 <sarais02@ucm.es>
2022-03-21 15:37:00 +0100

    Arreglando la pausa

--- MonkeyDelivery/Src/Control/States/PauseState.cpp
+++ MonkeyDelivery/Src/Control/States/PauseState.cpp
@@ -2,22 +2,15 @@
 #include "../PauseCommand.h"
 #include "../CommandExit.h"
 #include "../CommandClick.h"
-<<<<<<< Updated upstream
 
-=======
 #include "../UI/Buttons/Options.h"
->>>>>>> Stashed changes
 
 PauseState::PauseState(Game* game) : State(game)
 {
 	registerCommands();
-<<<<<<< Updated upstream
-	
-
-=======
 
 	addButton(new Options(game->getWindowWidth() / 2 - 45, game->getWindowHeight()/2, 100, 75, game));
->>>>>>> Stashed changes
+
 }
 
 void PauseState::update()
@@ -26,10 +19,9 @@ void PauseState::update()
 
 void PauseState::draw()
 {
-<<<<<<< Updated upstream
-=======
+
 	game->renderText("PAUSED", game->getWindowWidth() / 2 - 65, game->getWindowHeight() / 2 - 150);
->>>>>>> Stashed changes
+
 	for (auto b : getButtonsUI()) {
 		b->draw();
 	}
@@ -37,24 +29,12 @@ void PauseState::draw()
 
 void PauseState::next()
 {
-<<<<<<< Updated upstream
-	delete this;
-=======
-	for (auto b : buttonsUI) {
-		delete b;
-		b = nullptr;
-	}
->>>>>>> Stashed changes
 }
 
 void PauseState::registerCommands()
 {
-<<<<<<< Updated upstream
-	//commandFactory->add(new PauseCommand());
-=======
 	commandFactory->add(new PauseCommand());
 	commandFactory->add(new CommandClick());
->>>>>>> Stashed changes
+
 	commandFactory->add(new CommandExit());
-	commandFactory->add(new CommandClick());
 }
