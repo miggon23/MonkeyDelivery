89fd98a459bfcd0c62da70a8372d49152cc7f23b
Javier <javcomas@ucm.es>
2022-03-21 17:12:29 +0100

    slider

--- MonkeyDelivery/Src/Control/States/OptionsState.cpp
+++ MonkeyDelivery/Src/Control/States/OptionsState.cpp
@@ -8,6 +8,7 @@ OptionsState::OptionsState(Game* game) : State(game)
 {
 	registerCommands();
 	addButton(new Back(game->getWindowWidth() / 2 - 100, game->getWindowHeight() - 250, 100, 75, game, 1));
+	slider = new Slider(game, 200, 200);
 }
 
 void OptionsState::update()
@@ -16,7 +17,7 @@ void OptionsState::update()
 
 void OptionsState::draw()
 {
-
+	slider->draw();
 	for (auto b : getButtonsUI()) {
 		b->draw();
 	}
--- MonkeyDelivery/Src/Control/States/OptionsState.h
+++ MonkeyDelivery/Src/Control/States/OptionsState.h
@@ -2,9 +2,10 @@
 #include "State.h"
 #include "../../Logic/Game.h"
 #include "../States/MenuState.h"
-
+#include "../UI/Sliders/Slider.h"
 class OptionsState : public State
 {
+	Slider* slider;
 public:
 	OptionsState(Game* game);
 	~OptionsState() = default;
