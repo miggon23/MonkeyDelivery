3759b5a14a7d019974eef12573a01bfaf02b2e68
sarais02 <sarais02@ucm.es>
2022-03-21 16:16:40 +0100

    El menu de pausa ya tiene todos los botones

--- MonkeyDelivery/Src/Control/UI/Buttons/Continue.cpp
+++ MonkeyDelivery/Src/Control/UI/Buttons/Continue.cpp
@@ -1,1 +1,23 @@
 #include "Continue.h"
+
+#include "../../../Logic/Game.h"
+#include "../../States/PlayingState.h"
+
+Continue::Continue(int x, int y, int w, int h, Game* game) :Button(game, w, h, x, y)
+{
+	setTexture(backButtonTexture);
+}
+
+void Continue::onCursorCollision()
+{
+	//game->setState(new PlayingState(game));
+	if (game->getSavedState() != nullptr) {
+		State* tmp = game->getState();
+		State* saved = game->getSavedState();
+		saved->resetInitTime();
+		saved->registerCommands();
+		game->setState(saved);
+		game->clearSavedState();
+		delete tmp;
+	}
+}
