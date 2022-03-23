803743f25da3d0a11e7a2da50b767b146ecbefe4
Adrián Montero <82326212+admont02@users.noreply.github.com>
2022-03-23 02:00:01 +0100

    al dormir se te coloca en la cama :strawberry:

--- MonkeyDelivery/Src/Logic/Bed.cpp
+++ MonkeyDelivery/Src/Logic/Bed.cpp
@@ -15,4 +15,7 @@ void Bed::update()
 //lllamo al metodo para ver si puede dromir
 void Bed::onPlayerInteraction(Player* player){
 	player->changeSleep();
+
+	if(player->isAsleep())//si te vas a dormir
+	player->setPosition(getX()+15, getY());//colocar en la cama
 }
