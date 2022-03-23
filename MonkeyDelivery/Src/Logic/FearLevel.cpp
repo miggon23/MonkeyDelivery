6e513081d7c0f32b3ab1581a240a5e5d9c088da1
Elisa <elitodd@ucm.es>
2022-03-23 01:21:34 +0100

    Presentacion

--- MonkeyDelivery/Src/Logic/FearLevel.cpp
+++ MonkeyDelivery/Src/Logic/FearLevel.cpp
@@ -7,7 +7,7 @@ FearLevel::FearLevel(Game* game) :GameObject(game) {
 	fear_ = 0;
 	maxFear_ = 100;
 	setTexture(fearLevelTexture);
-	setPosition(place_, 50);
+	setPosition(place_, 70);
 	setDimension(400, 0);
 }
 
@@ -18,7 +18,7 @@ bool FearLevel::getScared(int amount)
 	if (fear_ < 0)fear_ = 0;
 	if (fear_ <maxFear_) {
 		setDimension(fear_*2, 50);
-		setPosition(50 - (amount/2), 50);
+		setPosition(50 - (amount/2), 70);
 		return false;
 	}
 	else {
