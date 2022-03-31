760e5b2cdfdf1f51f1489b8624f7fa5d28718672
Jacobo0601 <jacoboal@ucm.es>
2022-03-31 13:46:45 +0200

    af

--- MonkeyDelivery/Src/Logic/energyLevel.cpp
+++ MonkeyDelivery/Src/Logic/energyLevel.cpp
@@ -7,26 +7,27 @@ energyLevel::energyLevel(Game* game) :GameObject(game) {
 	energy_ = 200;
 	maxEnergy_ = 200;
 	setTexture(energyLevelTexture);
-	setPosition(place_, 10);
-	setDimension(200, 50);
+	setPosition(0, 0);
+	setDimension(108*3.1, 42*3.1);
 }
 
 // Drena energía y devuelve true si se queda a 0
 bool energyLevel::drain(float energyDrained)
 {
 	energy_ -= energyDrained;
-	if (energy_ > maxEnergy_)energy_ = maxEnergy_;
-	if (energy_ > 0) {
-		setDimension(energy_, 50);
-		setPosition(50 - (energyDrained/2), 10);
-		return false;
-	}
-	else {
-		energy_ = 0;
-		// avisa al player de que está sin energía
-		//cout << "ME DUERMO" << endl;
-		return true;
-	}
+	//if (energy_ > maxEnergy_)energy_ = maxEnergy_;
+	//if (energy_ > 0) {
+	//	setDimension(energy_, 50);
+	//	setPosition(50 - (energyDrained/2), 10);
+	//	return false;
+	//}
+	//else {
+	//	energy_ = 0;
+	//	// avisa al player de que está sin energía
+	//	//cout << "ME DUERMO" << endl;
+	//	return true;
+	//}
+	return true;
 }
 
 
