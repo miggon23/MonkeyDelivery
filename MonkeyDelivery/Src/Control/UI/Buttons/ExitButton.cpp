747537c877c0ed96449b4aec0d3be709508160d8
ssimoanto <simoanto@ucm.es>
2022-04-04 15:27:08 +0200

    boton salir

--- MonkeyDelivery/Src/Control/UI/Buttons/ExitButton.cpp
+++ MonkeyDelivery/Src/Control/UI/Buttons/ExitButton.cpp
@@ -1,7 +1,7 @@
 #include "ExitButton.h"
 
 ExitButton::ExitButton(int x, int y, int w, int h, Game* game):Button(game, w, h, x, y){
-	setTexture(pauseButtonTexture);
+	setTexture(exitButtonTexture);
 }
 
 void ExitButton::onCursorCollision(){
