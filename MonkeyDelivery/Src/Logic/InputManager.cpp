7769cb36fc1908b2a475c78eb9d3679e51a3b1db
admont02 <admont02@ucm.es>
2022-04-04 16:04:54 +0200

    borrando basurita :bicyclist:

--- MonkeyDelivery/Src/Logic/InputManager.cpp
+++ MonkeyDelivery/Src/Logic/InputManager.cpp
@@ -10,13 +10,10 @@ InputManager::InputManager(Game* _game)
 {
 	game = _game;
 	std::cout << "[DEBUG] frame duration: " << frameDuration() << " ms" << endl;
-	//initSDL();
 	IMG_Init(IMG_INIT_PNG);
 	renderer = game->getRenderer();
 	game->loadTextures();
 	
-	//commandFactory = new CommandFactory(game);
-	joystickDeadZone_ = 8000;
 	game->setState(new MenuState(game));
 	game->initOptionsState();
 	timer_ = Timer::Instance();
@@ -27,8 +24,6 @@ InputManager::~InputManager()
 {
 	timer_->Release();
 	timer_ = nullptr;
-	SDL_JoystickClose(gamepad_);
-	gamepad_ = NULL;
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
@@ -37,12 +32,11 @@ InputManager::~InputManager()
 
 void InputManager::run()
 {
-	//auto& ihdlr = ih();
 	while (!game->getState()->doQuit()) {
 		timer_->Update();
 
-		handleEvents();
-//		ihdlr.refresh();
+		auto state = game->getState();
+		state->handleEvents();
 
 		if (timer_->DeltaTime() >= 1.0f / FRAME_RATE) {
 			timer_->Reset();
@@ -53,72 +47,6 @@ void InputManager::run()
 		}
 
 	}
-}
-
-void InputManager::initSDL()
-{
-	SDL_Init(SDL_INIT_EVERYTHING);
-	//comprobacion joystick
-	if (SDL_NumJoysticks() < 1)
-	{
-		printf("Warning: No joysticks connected!\n");
-	}
-	else
-	{
-		//asociar joystick
-		gamepad_ = SDL_JoystickOpen(0);
-		if (gamepad_ == NULL)
-		{
-			printf("Warning: Unable to open game controller! SDL Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			printf("Name: %s\n", SDL_JoystickNameForIndex(0));
-		}
-	}
-
-
-	window = SDL_CreateWindow(game->getGameName().c_str(),
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		game->getWindowWidth(),
-		game->getWindowHeight(),
-		SDL_WINDOW_SHOWN);
-
-
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-	if (window == nullptr || renderer == nullptr)
-		printf("Window or renderer could not be created! SDL Error: %s\n", SDL_GetError());
-
-	SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
-	SDL_FillRect(screenSurface, &screenSurface->clip_rect, 0xFF);
-	SDL_UpdateWindowSurface(window);
-
-}
-
-void InputManager::handleEvents()
-{
-	auto state = game->getState();
-	game->getState()->handleEvents();
-	//SDL_Event event;
-	// /*Método simple de eventos*/
-	//while (SDL_PollEvent(&event)) {
-		/*if(state==MenuState)*/
-		/*Command* command = commandFactory->getCommand(event);
-		if (command != nullptr) {
-			command->execute();
-			break;
-		}
-		if (event.type == SDL_JOYAXISMOTION) {
-			printf("axis: %i %i\n", event.jaxis.axis, event.jaxis.value);
-		}
-		else if (event.type == SDL_JOYBUTTONDOWN) {
-			printf("button: %i\n", event.jbutton.button);
-		}*/
-
-	/*}*/
-
 }
 
 vector<SDL_Event>& InputManager::GetFrameEvents()
