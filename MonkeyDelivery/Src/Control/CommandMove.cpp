2cb1600e8d54dc34fab7ec417469e5622f5306a5
Adrián Montero <82326212+admont02@users.noreply.github.com>
2022-04-16 19:22:36 +0200

    movimiento simult. con mando y teclado

--- MonkeyDelivery/Src/Control/CommandMove.cpp
+++ MonkeyDelivery/Src/Control/CommandMove.cpp
@@ -4,41 +4,18 @@
 bool CommandMove::parse(SDL_Event& event)
 {
 	auto& ihdlr = ih();
-	//sin mando
-	if (!ihdlr.bJoysticksInitialised()) {
-		if (ihdlr.keyDownEvent()) {
-			//eje x
-			if (ihdlr.isKeyDown(SDL_SCANCODE_A))
-				game->setPlayerDirX(-1);
-			else if (ihdlr.isKeyDown(SDL_SCANCODE_D))
-				game->setPlayerDirX(1);
-			else game->setPlayerDirX(0);
-			//eje y
-			if (ihdlr.isKeyDown(SDL_SCANCODE_S))
-				game->setPlayerDirY(1);
-			else if (ihdlr.isKeyDown(SDL_SCANCODE_W))
-				game->setPlayerDirY(-1);
-			else game->setPlayerDirY(0);
-		}
-	}
-	//con mando
-	else
-	{
-		if (ihdlr.xvalue(0, 0) == -1)
-			game->setPlayerDirX(-1);
-		else if (ihdlr.xvalue(0, 0) == 1)
-			game->setPlayerDirX(1);
-		else game->setPlayerDirX(0);
 
-		if (ihdlr.yvalue(0, 0) == -1)
-			game->setPlayerDirY(-1);
-		else if (ihdlr.yvalue(0, 0) == 1)
-			game->setPlayerDirY(1);
-		else game->setPlayerDirY(0);
+	if (ihdlr.xvalue(0, 0) == -1 || ihdlr.isKeyDown(SDL_SCANCODE_A))
+		game->setPlayerDirX(-1);
+	else if (ihdlr.xvalue(0, 0) == 1 || ihdlr.isKeyDown(SDL_SCANCODE_D))
+		game->setPlayerDirX(1);
+	else game->setPlayerDirX(0);
 
-
-
-	}
+	if (ihdlr.yvalue(0, 0) == -1 || ihdlr.isKeyDown(SDL_SCANCODE_W))
+		game->setPlayerDirY(-1);
+	else if (ihdlr.yvalue(0, 0) == 1 || ihdlr.isKeyDown(SDL_SCANCODE_S))
+		game->setPlayerDirY(1);
+	else game->setPlayerDirY(0);
 
 	return false;
 }
