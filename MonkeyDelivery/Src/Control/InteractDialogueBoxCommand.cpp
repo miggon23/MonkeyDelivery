f1ed7bf214e5968271e1dfe651e22bf4d4accc2a
Javier <javcomas@ucm.es>
2022-04-22 16:50:44 +0200

    Filtros aun kedan unas clases por meter esta a mitad

---
+++ MonkeyDelivery/Src/Control/InteractDialogueBoxCommand.cpp
@@ -0,0 +1,17 @@
+#include "InteractDialogueBoxCommand.h"
+
+bool InteractDialogueBoxCommand::parse(SDL_Event& event)
+{
+	if (event.type == SDL_KEYDOWN) {
+		SDL_Keycode key = event.key.keysym.sym;
+		if (key == SDLK_SPACE) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void InteractDialogueBoxCommand::execute()
+{
+	game->interactDialogue();
+}
