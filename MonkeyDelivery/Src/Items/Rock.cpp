835c37030e698dbf54d3e5cb03d2f1c744121d11
Elisa <elitodd@ucm.es>
2022-04-28 23:17:58 +0200

    Bug rocas

--- MonkeyDelivery/Src/Items/Rock.cpp
+++ MonkeyDelivery/Src/Items/Rock.cpp
@@ -1,7 +1,7 @@
 #include "Rock.h"
 #include "../Logic/game.h"
 
-Rock::Rock(Game* game, int x, int y,int levelToDestroy) :ColliderTile(game,Vector2D<double>(x,y),w,h) {
+Rock::Rock(Game* game, int x, int y,int levelToDestroy) : ColliderTile(game,Vector2D<double>(x,y), 75.0, 75.0) {
 	levelToDestroy_ = levelToDestroy % 2;
 	switch (levelToDestroy_){
 	case 1:
@@ -11,8 +11,6 @@ Rock::Rock(Game* game, int x, int y,int levelToDestroy) :ColliderTile(game,Vecto
 		setTexture(worldObject_blockRock02);
 		break;
 	}
-	setDimension(w, h);
-	setPosition(x, y);
 }
 
 Rock::~Rock(){
--- MonkeyDelivery/Src/Items/Rock.h
+++ MonkeyDelivery/Src/Items/Rock.h
@@ -2,8 +2,8 @@
 #include "../Logic/ColliderTile.h"
 
 class Rock : public ColliderTile {
-	double w=75.0;
-	double h=75.0;
+	const double width_= 75.0;
+	const double height = 75.0;
 	int levelToDestroy_;
 public:
 	Rock(Game* game, int x, int y,int level);
