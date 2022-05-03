2024bb32c869ad39cd71e7b0350cb142c82bdb15
Jacobo0601 <82810110+Jacobo0601@users.noreply.github.com>
2022-05-03 12:08:21 +0200

    HUD con fondo

--- MonkeyDelivery/Src/Player/playerHUD.cpp
+++ MonkeyDelivery/Src/Player/playerHUD.cpp
@@ -1,14 +1,21 @@
 #include "playerHUD.h"
 #include "../Logic/Game.h"
 
-playerHUD::playerHUD(Game* game) :GameObject(game) {
+playerHUD::playerHUD(Game* game) :GameObject(game) 
+{
 	this->game = game;
-	setTexture(UI_playerHUD);
+	HUD_Base = game->getTexture(UI_playerHUD);
+	HUD_Fill = game->getTexture(UI_playerHUD_BackFill);
 	setPosition(20, 20);
-	setDimension(game->getWindowWidth()/4.0, game->getWindowHeight()/6.01);
+	//setDimension(game->getWindowWidth() / 4.0, game->getWindowHeight() / 6.01);
 }
 
-void playerHUD::draw()
+void playerHUD::drawFillHUD()
 {
-	drawTexture(texture);
+	baseRect_ = { 20, 20 , 113 * 4, 41 * 4 };
+	HUD_Fill->render(baseRect_);
+}void playerHUD::drawOverHUD()
+{
+	baseRect_ = { 20, 20 , 113 * 4, 41 * 4 };
+	HUD_Base->render(baseRect_);
 }
